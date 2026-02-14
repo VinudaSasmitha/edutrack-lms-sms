4a157ae47fc8a44ea3e871028592d3cee7afa15d
VinudaSasmitha <vinudasasmitha29@gmail.com>
2026-02-14 20:42:35 +0530

    Fire base input

--- admin-settings.js
+++ admin-settings.js
@@ -1,1 +1,14 @@
+        import { initializeApp } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-app.js";
+        import {
+            getAuth,
+            onAuthStateChanged,
+            signOut,
+            updateProfile
+        } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-auth.js";
+        import {
+            getFirestore,
+            doc,
+            getDoc,
+            setDoc
+        } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-firestore.js";
 
